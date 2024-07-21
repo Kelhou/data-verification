6ca2310d2afd1b82fa77a88844073af14c93ee70
Kelhou <kelhou000@gmail.com>
2024-07-21 14:32:05 +0530

    remove text

--- app.py
+++ app.py
@@ -130,6 +130,3 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-if __name__ == '__main__':
-    st.write('User Login and Data Update App')
